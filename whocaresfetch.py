0b4f3476f48af52b90cf556faf42b1de4923e477
sancalab <115105482+sancalab@users.noreply.github.com>
2024-01-07 14:47:16 +0100

    Update whocaresfetch.py

--- whocaresfetch.py
+++ whocaresfetch.py
@@ -1,5 +1,4 @@
 import platform
-import distro
 import os
 
 def get_terminal_info():
@@ -22,10 +21,10 @@ def colorize_text(text, color):
     }
     return f"{colors[color]}{text}{colors['ENDC']}"
 
-def whocaresfetch():
+def os_fetch():
     system_info = platform.uname()
 
-    print(colorize_text("whocaresfetch", 'OKBLUE'))
+    print(colorize_text("OS Fetch", 'OKGREEN'))
     print(colorize_text(f"System: {system_info.system}", 'OKCYAN'))
     print(colorize_text(f"Node: {system_info.node}", 'OKCYAN'))
     print(colorize_text(f"Release: {system_info.release}", 'OKCYAN'))
@@ -34,9 +33,8 @@ def whocaresfetch():
     print(colorize_text(f"Processor: {system_info.processor}", 'OKCYAN'))
 
     print(colorize_text("\nDistribution Information:", 'OKGREEN'))
-    dist_info = distro.info()
-    print(colorize_text(f"Distribution: {dist_info['id']} {dist_info['version']}", 'OKCYAN'))
-    print(colorize_text(f"Codename: {dist_info['codename']}", 'OKCYAN'))
+    dist_info = platform.platform()
+    print(colorize_text(f"Distribution: {dist_info}", 'OKCYAN'))
     
     print(colorize_text("\nAdditional Information:", 'OKGREEN'))
     print(colorize_text(f"Architecture: {platform.architecture()}", 'OKCYAN'))
@@ -67,4 +65,4 @@ def whocaresfetch():
     print(colorize_text(f"Terminal Size: {term_size.columns} columns x {term_size.lines}", 'OKCYAN'))
 
 if __name__ == "__main__":
-    whocaresfetch()
+    os_fetch()
